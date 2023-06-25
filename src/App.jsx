677ca8908052c78877dfd4de14a0a79eb345d5f1
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2023-06-24 18:04:55 -0600

    routes added, data cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import { Features } from "./components/features";
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
+import ErrorPage from "./error-page";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
 });
+
+
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
@@ -23,17 +25,40 @@ const App = () => {
     setLandingPageData(JsonData);
   }, []);
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Header data={landingPageData.Header} />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/about",
+      element:  <About data={landingPageData.About} />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/services",
+      element:  <Services data={landingPageData.Services} />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/inquiry",
+      element:  <Contact data={landingPageData.Contact} />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
   return (
     <div>
       <Navigation />
+      <RouterProvider router={router} />
+      {/* 
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+      <Contact data={landingPageData.Contact} /> */}
     </div>
   );
 };
