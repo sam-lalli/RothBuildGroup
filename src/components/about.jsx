78d4706950e15daf94623ff14784ab27edf29b8f
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2023-10-20 00:32:27 -0600

    rebrand to high west management - also migrated to fa 4.7

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,7 +11,7 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
+              <h2>About Hunter Roth, Founder of High West Estate Management</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <p>{props.data ? props.data.paragraph2 : "loading..."}</p>
             </div>
@@ -21,7 +21,7 @@ export const About = (props) => {
         <div className="col-xs-12 col-md-6">
           {" "}
             <div className="about-text-2">
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <p>{props.data ? props.data.paragraph3 : "loading..."}</p>
                             <h3>Why Us?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
@@ -46,7 +46,7 @@ export const About = (props) => {
       </div>
       </div>{" "}
           <div className="col-xs-12 col-md-6">
-            <img src="img/about.jpg" className="img-responsive bottom-img" alt="" />
+            <img src="img/about2.jpg" className="img-responsive bottom-img" alt="" />
           </div>
       </div>
     </div>
